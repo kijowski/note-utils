61f1e330359010b2b612e01d812bddf8de99119f
Michal Kijowski <contakt@michalkijowski.pl>
2021-04-23 17:17:59 +0200

    Add getSelectionHTML function

--- surfkeys/settings.js
+++ surfkeys/settings.js
@@ -35,6 +35,33 @@ removeSearchAliasX("baidu", "b");
 removeSearchAliasX("bing", "w");
 removeSearchAliasX("bing", "w");
 
+function getSelectionHTML() {
+  var userSelection;
+  if (window.getSelection) {
+    // W3C Ranges
+    userSelection = window.getSelection();
+    // Get the range:
+    if (userSelection.getRangeAt) {
+      var range = userSelection.getRangeAt(0);
+    } else {
+      var range = document.createRange();
+      range.setStart(userSelection.anchorNode, userSelection.anchorOffset);
+      range.setEnd(userSelection.focusNode, userSelection.focusOffset);
+    }
+    // And the HTML:
+    var clonedSelection = range.cloneContents();
+    var div = document.createElement("div");
+    div.appendChild(clonedSelection);
+    return div.innerHTML;
+  } else if (document.selection) {
+    // Explorer selection, return the HTML
+    userSelection = document.selection.createRange();
+    return userSelection.htmlText;
+  } else {
+    return "";
+  }
+}
+
 function capture(body) {
   const url = location.href;
   const title = document.title;
